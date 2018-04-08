fd1e18d3386fb70680ff11dc0bce0b214805f216
Vuong Dao <duyvuong.uit@gmail.com>
2018-04-08 15:13:03 +0800

    initialized project

---
+++ DemoGraphQL/Pods/Target Support Files/Pods-DemoGraphQL/Pods-DemoGraphQL-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DemoGraphQL : NSObject
+@end
+@implementation PodsDummy_Pods_DemoGraphQL
+@end
---
+++ DemoGraphQL/Pods/Target Support Files/Pods-DemoGraphQLTests/Pods-DemoGraphQLTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DemoGraphQLTests : NSObject
+@end
+@implementation PodsDummy_Pods_DemoGraphQLTests
+@end
